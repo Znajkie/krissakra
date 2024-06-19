695eeac5215d3434f6b3650195d9645f62429ea0
Christian <explosivfibres@hotmail.com>
2024-06-19 21:42:58 +0200

    site update

--- main.js
+++ main.js
@@ -1,29 +1,73 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const btn = document.querySelector('.email');
+  const emailBtn = document.querySelector('.email');
+  const bookingBtn = document.querySelector('.bookingBtn');
+  const footerContactLink = document.querySelector('.footer-contact-link');
   const contactWrapper = document.querySelector('.contact__wrapper');
   const closeBtn = document.querySelector('.close-btn'); // Select the close button
 
-  btn.addEventListener('click', function () {
+  // Function to show the contact form
+  function showContactForm() {
     contactWrapper.style.display = 'flex'; // Show the popup
-  });
+  }
 
-  closeBtn.addEventListener('click', function () {
+  // Function to hide the contact form
+  function hideContactForm() {
     contactWrapper.style.display = 'none'; // Hide the popup when close button is clicked
-  });
+  }
+
+  // Add click event listener to email button
+  emailBtn.addEventListener('click', showContactForm);
+
+  bookingBtn.addEventListener('click', showContactForm);
+
+  // Add click event listener to footer contact link
+  footerContactLink.addEventListener('click', showContactForm);
+
+  // Add click event listener to close button
+  closeBtn.addEventListener('click', hideContactForm);
 
   // Optional: Close the popup by clicking outside the form
   contactWrapper.addEventListener('click', function (e) {
     if (e.target === contactWrapper) {
       // Check if the clicked area is outside the form
-      contactWrapper.style.display = 'none';
+      hideContactForm();
     }
   });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+
+  const bookingBtn = document.querySelector('.bookingBtn');
+  const contactWrapper = document.querySelector('.contact__wrapper');
+  const closeBtn = document.querySelector('.close-btn'); // Select the close button
+
+  // Function to show the contact form
+  function showContactForm() {
+    contactWrapper.style.display = 'flex'; // Show the popup
+  }
+
+  // Function to hide the contact form
+  function hideContactForm() {
+    contactWrapper.style.display = 'none'; // Hide the popup when close button is clicked
+  }
+
+  // Add click event listener to email button
+
+  bookingBtn.addEventListener('click', showContactForm);
 
 
+  // Add click event listener to close button
+  closeBtn.addEventListener('click', hideContactForm);
+
+  // Optional: Close the popup by clicking outside the form
+  contactWrapper.addEventListener('click', function (e) {
+    if (e.target === contactWrapper) {
+      // Check if the clicked area is outside the form
+      hideContactForm();
+    }
+  });
+});
 var navToTopBtn = document.getElementById('nav-to-top');
-
 // Listen for scroll events on the window
 window.addEventListener('scroll', function () {
   // Check if page is scrolled more than 50 pixels (for example)
@@ -36,12 +80,12 @@ window.addEventListener('scroll', function () {
   }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var navItems = document.querySelectorAll('.underline');
 
-  navItems.forEach(function(item) {
-    item.addEventListener('click', function() {
-      navItems.forEach(function(nav) {
+  navItems.forEach(function (item) {
+    item.addEventListener('click', function () {
+      navItems.forEach(function (nav) {
         nav.classList.remove('active');
       });
 
@@ -52,23 +96,23 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  let subtitles = document.querySelectorAll(
-    '.subtitel-text, .subtitel-text__bottom'
+  let titles = document.querySelectorAll(
+    '.title-text, .title-text__bottom'
   );
-  let index = 0; // To keep track of the current subtitle
+  let index = 0; // To keep track of the current title
 
-  function animateSubtitle() {
-    if (index >= subtitles.length) return; // Stop if we've animated all subtitles
+  function animateTitle() {
+    if (index >= titles.length) return; // Stop if we've animated all titles
 
-    let subtitle = subtitles[index];
-    subtitle.classList.add('fade-in');
+    let title = titles[index];
+    title.classList.add('fade-in');
 
-    // Wait for the fade-in to finish before moving to the next subtitle
+    // Wait for the fade-in to finish before moving to the next title
     setTimeout(() => {
-      index++; // Move to the next subtitle
-      animateSubtitle(); // Start the animation for the next subtitle
+      index++; // Move to the next title
+      animateTitle(); // Start the animation for the next title
     }, 1000 + 750); // Wait for 1s animation to finish + 500ms delay before the next
   }
 
-  animateSubtitle(); // Start the animation sequence
+  animateTitle(); // Start the animation sequence
 });
--- translation.js
+++ translation.js
@@ -4,88 +4,133 @@ i18next.init(
     debug: true,
     fallbackLng: 'sv',
     resources: {
+      //* Engelska
       en: {
         translation: {
+          //* Navbar
           mainLink1: 'Home',
           mainLink2: 'About us',
           mainLink3: 'Services',
           mainLink4: 'Why Krissäkra?',
           mainLink5: 'Contact',
-
-          subtitleText: 'Did you know that 80% of all crises are foreseeable?',
-          subtitleText2: 'Secure your building today, for a safer tomorrow!',
+          //* Header title
+          titleText: 'Did you know that 80% of all crises are foreseeable?',
+          titleText2: 'Secure your building today, for a safer tomorrow!',
+          //* Booking button
+          bookingBtn: 'Contact us!',
+          //* Why us
           whyUs: 'Why Krissäkra?',
           whyUs1:
-            'The current global situation has placed new and increased demands on society.',
+            'At Krissäkra Sverige AB, we offer the tools and knowledge needed for housing cooperatives and communities to navigate before, during, and after a crisis.',
           whyUs2:
-            'Securing your housing association (BRF) or community creates a strong and proactive foundation for effective crisis management and contingency planning, so that together you have the conditions and capabilities to face the future, no matter in what form it manifests.',
+            'The common framework and individually tailored action plan form a foundation and insurance that protects your collective and individual values.',
           whyUs3:
-            'With proper preparation and effective contingency planning, you have the ability to act when crisis strikes.',
+            'We realized that a new concept was needed to clarify our vision, Collective Preparedness.',
           whyUs4:
             'Krissäkra Sverige AB, offer the tools and knowledge required to prepare you to safely navigate before, during, and after a crisis.',
           whyUs5:
             'Strengthening both the individual and your group while safeguarding your shared values.',
           whyUs6: 'Think ahead and choose us for an investment in your future!',
-
+          //* About us
           aboutUs: 'Who are we at Krissäkra?',
           aboutUs1:
-            'The team at Krissäkra consists of three founders with diverse backgrounds and experiences, where we have combined academic expertise with practical experience to create a solid knowledge base and know-how.',
+            'The team at Krissäkra consists of three founders with different backgrounds and experiences. ',
           aboutUs2:
-            'At Krissäkra Sverige AB, we strive not only to strengthen individual capabilities but also to contribute to collective preparedness and crisis management capabilities.',
+            'We have successfully combined academic knowledge with practical experience to create a broad knowledge base and depth in our "know-how".',
           aboutUs3:
-            'Our team is dedicated to supporting businesses, housing associations, and communities by developing effective contingency plans and crisis management capabilities.',
+            'At Krissäkra Sverige AB, we strive not only to strengthen individual capability but also to promote collective preparedness and crisis management capability.',
           aboutUs4:
-            'Through our collective knowledge and experience, we create tailored solutions that prepare you for a safer and more stable future, both today and tomorrow.',
+            'Our passion and driving force is to support society by helping companies, housing cooperatives, and communities develop effective contingency plans and crisis management strategies.',
+          aboutUs5:
+            'With our combined knowledge and experience, we create tailored solutions that prepare you for a safer future in a rapidly changing world, helping you both today and tomorrow.',
+          aboutUs6:
+            'For us, preparedness should be simple, and with the right conditions, it is also possible.',
+
+          //* Product list
+          product_listh3: 'What does it mean to Krissäkra?',
+          product_list1_header: 'Site visit',
+          product_list2_header: 'RSA (Risk and Vulnerability Analysis)',
+          product_list3_header: 'Contingency plan',
+          product_list4_header: 'Collective preparedness',
+          product_list5_header: 'Equipment',
+          product_list6_header: 'Krissäkra certification',
+          product_list7_header: 'Safety and capability',
+          // product_list8_header: '',
         },
       },
+      //* Svenska
       sv: {
         translation: {
-          //NAVBAR SWE
+          //* Navigationsbar
           mainLink1: 'Hem',
           mainLink2: 'Om oss',
           mainLink3: 'Tjänster',
           mainLink4: 'Varför Krissäkra?',
           mainLink5: 'Kontakta',
 
-          //HEADER SWE
-          subtitleText:
+          //* Huvudrubrik
+          titleText:
             'Visste du att 80% av alla kriser var möjliga att förutse?',
-          subtitleText2:
-            'Krissäkra din fastighet idag, för en tryggare framtid imorgon!',
+          titleText2:
+            'Krissäkra er fastighet idag, för en tryggare framtid imorgon!',
+          //* Boka knapp
+          bookingBtn: 'Boka nu!',
 
-          // WHY US SECTION SWE
+          //* Varför oss
           whyUs: 'Varför välja att Krissäkra?',
           whyUs1:
-            'Det rådande omvärldsläget ställer nya och ökade krav på samhället.',
+            'Vi på Krissäkra Sverige AB erbjuder de verktyg och den kunskap som krävs för att bostadsrättsföreningar och samfälligheter ska kunna navigera före, under och efter en kris.',
           whyUs2:
-            'Att krissäkra din bostadsrättsförening (BRF) eller samfällighet skapar en stark och proaktiv grund för en effektiv krishantering och beredskapsplanering, vilket gör att ni tillsammans skapar förutsättningar för att möta framtiden, oavsett i vilken form den manifesterar sig.',
+            'Det gemensamma ramverket och den individuellt framtagna handlingsplanen utgör en grund och försäkring som skyddar era gemensamma och individuella värden.',
           whyUs3:
-            'Med rätt förberedelse och en effektiv beredskapsplanering har ni förmågan att agera om väl krisen slår till.',
+            'Vi insåg att det behövdes ett nytt begrepp för att tydliggöra vår vision, Samberedskap.',
           whyUs4:
             ' Vi på Krissäkra Sverige erbjuder de verktyg och den kunskap som krävs för ni ska kunna navigera före under och efter en kris, vilket stärker både individen och er som grupp.',
           whyUs5:
             'Den gemensamma beredskapen skapar en stabil grund för att skydda era gemensamma värden.',
           whyUs6:
             'Tänk efter före och välj oss för en investering som gör skillnad.',
-          // ABOUT US SECTION SWE
+          //* Om oss
           aboutUs: 'Vilka är vi på Krissäkra?',
           aboutUs1:
-            'Teamet på Krissäkra består av tre grundare med olika bakgrunder och erfarenheter där vi kombinerat akademisk expertis med praktisk erfarenhet för att skapa en stabil kunskapsbas och ett ”know how”.',
+            'Teamet på Krissäkra utgörs av tre grundare med olika bakgrunder och erfarenheter.',
           aboutUs2:
-            'På Krissäkra Sverige AB strävar vi efter att inte bara stärka individuell förmåga utan också bidra till kollektiv beredskap och krishanteringsförmåga.',
+            'Vi har framgångsrikt kombinerat akademisk kunskap med praktisk erfarenhet för att skapa en bred kunskapsbas och ett djup i vårt "know-how".',
           aboutUs3:
-            'Vårt team brinner för att stödja företag, bostadsrättsföreningar och samfälligheter genom att utveckla effektiva beredskapsplaner och krisledningsförmågor.',
+            'På Krissäkra Sverige AB strävar vi inte bara efter att stärka individuell förmåga utan även att främja kollektiv beredskap och krishanteringsförmåga.',
           aboutUs4:
-            'Genom vår samlade kunskap och erfarenhet skapar vi skräddarsydda lösningar som förbereder er för en tryggare och mer stabil framtid, både idag och imorgon.',
+            'Vår passion och drivkraft är att stötta samhället genom att hjälpa företag, bostadsrättsföreningar och samfälligheter att utveckla effektiva beredskapsplaner och krisledningsstrategier.',
+          aboutUs5:
+            'Med vår samlade kunskap och erfarenhet skapar vi skräddarsydda lösningar som förbereder er för en tryggare framtid i en snabbt föränderlig omvärld vilket hjälper er både idag och imorgon.',
+          aboutUs6:
+            'För oss ska beredskap vara enkelt, och med rätt förutsättningar är det också möjligt.',
+
+          //* Produktlista
+          product_listh3: 'Vilka steg tar vi för att Krissäkra just er?',
+          product_list1_header: 'Platsbesök',
+          product_list2_header: 'RSA (Risk och sårbarhetsanalys)',
+          product_list3_header: 'Beredskapsplan',
+          product_list4_header: 'Samberedskap',
+          product_list5_header: 'Utrustning',
+          product_list6_header: 'Krissäkra certifiering',
+          product_list7_header: 'Trygghet och förmåga',
+          // product_list8_header: '',
         },
       },
     },
   },
   function (err, t) {
-    updateContent();
+    // Language initialization callback
+    if (err) {
+      console.error('Error initializing i18next:', err);
+      return;
+    }
+
+    updateContent(); // Initial content update
   }
 );
 
+// Function to update content based on translation
 function updateContent() {
   document.querySelectorAll('[data-i18n]').forEach(function (element) {
     var key = element.getAttribute('data-i18n');
@@ -93,20 +138,24 @@ function updateContent() {
   });
 }
 
+// Function to change language
 function changeLanguage(lng) {
-  i18next.changeLanguage(lng, function () {
-    updateContent();
-    updateLanguageButtonText(lng);
-  });
-
-  function updateLanguageButtonText(currentLang) {
-    const btn = document.getElementById('languageBtn');
-    if (currentLang === 'sv') {
-      btn.innerText = 'English';
-      btn.setAttribute('onclick', "changeLanguage('en')");
-    } else {
-      btn.innerText = 'Svenska';
-      btn.setAttribute('onclick', "changeLanguage('sv')");
+  i18next.changeLanguage(lng, function (err, t) {
+    if (err) {
+      console.error('Error changing language:', err);
+      return;
     }
+    updateContent(); // Update content after language change
+    updateLanguageButtonText(lng); // Update language button text
+  });
+}
+
+// Function to update language button text
+function updateLanguageButtonText(currentLang) {
+  const btn = document.getElementById('languageBtn');
+  if (currentLang === 'sv') {
+    btn.setAttribute('onclick', "changeLanguage('en')");
+  } else {
+    btn.setAttribute('onclick', "changeLanguage('sv')");
   }
 }
